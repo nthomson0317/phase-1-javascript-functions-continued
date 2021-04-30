693cc924f68e3d6a4e0b03c2c46cbd54da7e7003
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-04-30 18:11:27 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(skate='roller-skate') {
+    return (`This Saturday, I want to ${skate}!`)
+}
+
+const mondayWork = function(will ='go to the office'){
+    return (`This Monday, I will ${will}.`)
+}
+
+function wrapAdjective(flair='*'){
+return function(parameter='special'){
+    return `You are ${flair}${parameter}${flair}!`
+}}
+
